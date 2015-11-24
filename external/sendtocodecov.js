d058d44ad295487574372b0371443d721cb65cb7
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-25 00:50:16 +0100

    test is working locally - let's see what travis and codecov says

---
+++ external/sendtocodecov.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var path = require('path');
+
+fs.readFile('coverage/coverage.json', 'utf8', function(err, data) {
+	if(err) // coverage data not found/read
+		console.error(err), process.exit(1);
+	pipeToCodeCov(data);
+	process.exit(0);
+});
+
+function pipeToCodeCov(json) {
+	var codecov = 'node_modules/.bin/codecov',
+			child;
+
+	child = spawn(process.execPath, [path.resolve(codecov)], { stdio: ['pipe', 'ignore', 'ignore'] });
+	child.stdin.write(json);
+}
