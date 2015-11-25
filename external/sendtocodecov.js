8e1d9505992d7ad4648f353d3ecc88d49714f268
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-11-25 12:52:19 +0100

    exit if codecov is not found

--- external/sendtocodecov.js
+++ external/sendtocodecov.js
@@ -4,25 +4,26 @@ var fs = require('fs');
 var spawn = require('child_process').spawn;
 var path = require('path');
 
-fs.readFile(path.resolve(__dirname, '..','coverage/coverage.json'), 'utf8', function(err, data) {
-	if(err) {		
-		console.error(err);
-		console.log('coverage data not found/read');
-		process.exit(1);
-	}
-	pipeToCodeCov(data);
+fs.readFile(path.resolve(__dirname, '..', 'coverage/coverage.json'), 'utf8', function(err, data) {
+  if (err) {
+    console.error(err);
+    console.error('coverage data not found/read');
+    process.exit(1);
+  }
+  pipeToCodeCov(data);
 });
 
 function pipeToCodeCov(json) {
-	var codecov = 'node_modules/.bin/codecov';//'external/testsendtocodecov.js'//
-  var	child;
-	var node = process.execPath;
-	var codecov = path.resolve(__dirname, '..', codecov)
-	try {
-		child = spawn(node, [codecov]);
-	} catch (err) {
-		console.log('codecov lib is not accessible');
-	}
-	child.stdin.setEncoding = 'utf8';
-	child.stdin.end(json + '\n');
+  var codecov = 'node_modules/.bin/codecov';
+  var child;
+  var node = process.execPath;
+  var codecov = path.resolve(__dirname, '..', codecov)
+  try {
+    child = spawn(node, [codecov]);
+  } catch (err) {
+    console.error('codecov lib is not accessible');
+    process.exit()
+  }
+  child.stdin.setEncoding = 'utf8';
+  child.stdin.end(json + '\n');
 }
