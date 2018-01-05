4c648eee0349693cd4368605a4a442f5237cd6f2
George Gkasdrogkas <georgegkas@gmail.com>
2018-01-05 16:06:29 +0200

    Include spec/ folder to eslint
    Refers to issue: #28
    
    - Lint files under spec/ folder with eslint.
    - Add ES6 syntax to test files under spec/ folder to pass
    linter tests.
    
    This commit limits the ability of CIs that are configured to
    use node versions that do not support es6 basic features
    (like arrow functions and const, let keyword). In those
    enviroments the build will fail. In the future this has to
    be addressed either by updating the node version in CIs, or
    by switching back to ES5 code

--- spec/stream.spec.js
+++ spec/stream.spec.js
@@ -1,78 +1,76 @@
-/* globals describe, it, expect */
+'use strict';
 
-"use strict";
+const Stream = require('../src/stream.js');
 
-var Stream = require('../src/stream.js');
-
-describe('finite streams', function () {
-  it('can be made and accessess', function () {
-    var finite = Stream.make(10, 20, 30);
+describe('finite streams', () => {
+  it('can be made and accessess', () => {
+    const finite = Stream.make(10, 20, 30);
     expect(finite.length()).toBe(3);
     expect(finite.head()).toBe(10);
     expect(finite.item(0)).toBe(10);
     expect(finite.item(1)).toBe(20);
     expect(finite.item(2)).toBe(30);
   });
-  
-  it('has a head and tail', function () {
-    var ini = {};
-    var fin = {};
-    var duo = Stream.make(ini, fin);
-    
+
+  it('has a head and tail', () => {
+    const ini = {};
+    const fin = {};
+    const duo = Stream.make(ini, fin);
+
     expect(duo.head()).toBe(ini);
     expect(duo.tail().head()).toBe(fin);
     expect(duo.tail().tail().empty()).toBeTruthy();
   });
-  
-  it('detects membership', function () {
-    var stooges = Stream.make("Curly", "Moe", "Larry");
-    expect(stooges.member("Curly")).toBeTruthy();
-    expect(stooges.member("Bobert")).toBeFalsy();
+
+  it('detects membership', () => {
+    const stooges = Stream.make('Curly', 'Moe', 'Larry');
+    expect(stooges.member('Curly')).toBeTruthy();
+    expect(stooges.member('Bobert')).toBeFalsy();
   });
 
-  it("appends another stream", function() {
-    var s0 = new Stream();
-    var s1 = Stream.make(1);
-    var s2 = Stream.make(2, 3);
-    var appended_stream1 = s1.append(s2);
-    var appended_stream2 = s0.append(s2);
-    expect(appended_stream1.head()).toBe(1);
-    expect(appended_stream1.item(1)).toBe(2);
-    expect(appended_stream1.item(2)).toBe(3);
-    expect(appended_stream1.length()).toBe(3);
-    expect(s2).toBe( appended_stream2 );
+  it('appends another stream', () => {
+    const s0 = new Stream();
+    const s1 = Stream.make(1);
+    const s2 = Stream.make(2, 3);
+    const appendedStream1 = s1.append(s2);
+    const appendedStream2 = s0.append(s2);
+    expect(appendedStream1.head()).toBe(1);
+    expect(appendedStream1.item(1)).toBe(2);
+    expect(appendedStream1.item(2)).toBe(3);
+    expect(appendedStream1.length()).toBe(3);
+    expect(s2).toBe(appendedStream2);
   });
 
-  it("compares 2 streams for equality", function() {
-    var s1 = Stream.make(1);
-    var s2 = Stream.make(1);
-    var s3 = Stream.make(2, 3);
-    expect(Stream.equals(s1,s2)).toBeTruthy;
-    expect(Stream.equals(s1,s3)).toBeFalsy;
+  it('compares 2 streams for equality', () => {
+    const s1 = Stream.make(1);
+    const s2 = Stream.make(1);
+    const s3 = Stream.make(2, 3);
+    expect(Stream.equals(s1, s2)).toBeTruthy();
+    expect(Stream.equals(s1, s3)).toBeFalsy();
   });
 });
 
-describe('construction', function () {
-  it('can be created from array', function () {
-    var test_stream = Stream.fromArray([1, 2, 3]);
+describe('construction', () => {
+  it('can be created from array', () => {
+    const testStream = Stream.fromArray([1, 2, 3]);
 
-    expect(test_stream.head()).toBe(1);
-    expect(test_stream.item(1)).toBe(2);
-    expect(test_stream.item(2)).toBe(3);
-    expect(test_stream.length()).toBe(3);
+    expect(testStream.head()).toBe(1);
+    expect(testStream.item(1)).toBe(2);
+    expect(testStream.item(2)).toBe(3);
+    expect(testStream.length()).toBe(3);
   });
 
-  it('can be created with iterate', function () {
-    var powers_of_two = Stream.iterate(1, function (x) { return x * 2; });
+  it('can be created with iterate', () => {
+    const powersOfTwo = Stream.iterate(1, x => x * 2);
 
-    expect(powers_of_two.item(1)).toBe(2);
-    expect(powers_of_two.item(2)).toBe(4);
-    expect(powers_of_two.item(3)).toBe(8);
-    expect(powers_of_two.item(10)).toBe(1024);
+    expect(powersOfTwo.item(1)).toBe(2);
+    expect(powersOfTwo.item(2)).toBe(4);
+    expect(powersOfTwo.item(3)).toBe(8);
+    expect(powersOfTwo.item(10)).toBe(1024);
   });
 
-  it('can be created by applying cycle to an array', function() {
-    var s = Stream.cycle([98, 99, 100]);
+  it('can be created by applying cycle to an array', () => {
+    const s = Stream.cycle([98, 99, 100]);
 
     expect(s.head()).toBe(98);
     expect(s.item(1)).toBe(99);
@@ -84,8 +82,8 @@ describe('construction', function () {
     expect(s.item(32)).toBe(100);
   });
 
-  it('Can be created of an element', function() {
-    var s = Stream.repeat('element');
+  it('Can be created of an element', () => {
+    const s = Stream.repeat('element');
 
     expect(s.head()).toBe('element');
     expect(s.item(1)).toBe('element');
@@ -93,14 +91,14 @@ describe('construction', function () {
     expect(s.item(1024)).toBe('element');
   });
 
-  it('Memoizes properly', function() {
-    var calls = 0;
-    function count_calls() {
-      calls++;
-      return new Stream(1, count_calls);
+  it('Memoizes properly', () => {
+    let calls = 0;
+    function countCalls() {
+      calls += 1;
+      return new Stream(1, countCalls);
     }
 
-    var s = new Stream(1, count_calls);
+    const s = new Stream(1, countCalls);
 
     s.head();
     expect(calls, 0);
@@ -113,14 +111,14 @@ describe('construction', function () {
     expect(calls, 1);
   });
 
-  it('Doesn\'t memoize when we ask for Eager', function() {
-    var calls = 0;
-    function count_calls() {
-      calls++;
-      return new Stream.Eager(1, count_calls);
+  it('Doesn\'t memoize when we ask for Eager', () => {
+    let calls = 0;
+    function countCalls() {
+      calls += 1;
+      return new Stream.Eager(1, countCalls);
     }
 
-    var s = new Stream(1, count_calls);
+    const s = new Stream(1, countCalls);
 
     s.head();
     expect(calls, 0);
@@ -130,189 +128,179 @@ describe('construction', function () {
     expect(calls, 1);
 
     s.tail().head();
-    expect(calls,2);
+    expect(calls, 2);
   });
 });
 
-describe('range', function () {
-  it('can make ranges from a minimum value to a maximal', function () {
-    var three_to_seven = Stream.range(3, 7);
-    
-    expect(three_to_seven.length()).toBe(5);
-    expect(three_to_seven.item(0)).toBe(3);
-    expect(three_to_seven.item(1)).toBe(4);
-    expect(three_to_seven.item(2)).toBe(5);
-    expect(three_to_seven.item(3)).toBe(6);
-    expect(three_to_seven.item(4)).toBe(7);
+describe('range', () => {
+  it('can make ranges from a minimum value to a maximal', () => {
+    const threeToSeven = Stream.range(3, 7);
+
+    expect(threeToSeven.length()).toBe(5);
+    expect(threeToSeven.item(0)).toBe(3);
+    expect(threeToSeven.item(1)).toBe(4);
+    expect(threeToSeven.item(2)).toBe(5);
+    expect(threeToSeven.item(3)).toBe(6);
+    expect(threeToSeven.item(4)).toBe(7);
   });
-  
-  it('has an optional highest value', function () {
-    var ten_plus = Stream.range(10);
-    expect(ten_plus.head()).toBe(10);
-    expect(ten_plus.tail().head()).toBe(11);
+
+  it('has an optional highest value', () => {
+    const tenPlus = Stream.range(10);
+    expect(tenPlus.head()).toBe(10);
+    expect(tenPlus.tail().head()).toBe(11);
   });
-  
-  it('defaults to the natural numbers', function () {
-    var naturals = Stream.range();    
+
+  it('defaults to the natural numbers', () => {
+    const naturals = Stream.range();
     expect(naturals.head()).toBe(1);
     expect(naturals.tail().head()).toBe(2);
   });
 });
 
-describe('standard functional functions', function () {
-  it('takes', function () {
-    var naturals = Stream.range();
-    var first_three_naturals = naturals.take(3);
-    
-    expect(first_three_naturals instanceof Stream).toBeTruthy();
-    expect(first_three_naturals.length()).toBe(3);
-    expect(first_three_naturals.item(0)).toBe(1);
-    expect(first_three_naturals.item(1)).toBe(2);
-    expect(first_three_naturals.item(2)).toBe(3);
-    expect(function () {first_three_naturals.item(3);})
-      .toThrow()
+describe('standard functional functions', () => {
+  it('takes', () => {
+    const naturals = Stream.range();
+    const firstThreeNaturals = naturals.take(3);
+
+    expect(firstThreeNaturals instanceof Stream).toBeTruthy();
+    expect(firstThreeNaturals.length()).toBe(3);
+    expect(firstThreeNaturals.item(0)).toBe(1);
+    expect(firstThreeNaturals.item(1)).toBe(2);
+    expect(firstThreeNaturals.item(2)).toBe(3);
+    expect(() => { firstThreeNaturals.item(3); })
+      .toThrow();
   });
-  
-  it('drops', function () {
-    var oldComment = {};
-    var oldestComment = {};
-    var newComment = {};
-    var newestComment = {};
-    var comments = Stream.make(oldestComment, oldComment, newComment, newestComment);
-    var newComments = comments.drop(2);
+
+  it('drops', () => {
+    const oldComment = {};
+    const oldestComment = {};
+    const newComment = {};
+    const newestComment = {};
+    const comments = Stream.make(oldestComment, oldComment, newComment, newestComment);
+    const newComments = comments.drop(2);
     expect(newComments.length()).toBe(2);
     expect(newComments.member(oldComment)).toBeFalsy();
     expect(newComments.member(newComment)).toBeTruthy();
     expect(newComments.head()).toBe(newComment);
     expect(newComments.tail().head()).toBe(newestComment);
-  })
-  
-  it('maps', function () {
-    var alphabet_ascii = Stream.range(97, 122);
-    var alphabet = alphabet_ascii.map(function (code) {
-      return String.fromCharCode(code);
-    });
-    
+  });
+
+  it('maps', () => {
+    const alphabetAscii = Stream.range(97, 122);
+    const alphabet = alphabetAscii.map(code => String.fromCharCode(code));
+
     expect(alphabet.head()).toBe('a');
     expect(alphabet.tail().head()).toBe('b');
     expect(alphabet.item(25)).toBe('z');
   });
-  
-  it('filters', function () {
-    var first_ten_naturals = Stream.range(1, 10);
-    var first_five_evens = first_ten_naturals.filter(function (n) {
-      return (n % 2 === 0);
-    });
-    
-    expect(first_five_evens.length()).toBe(5);
-    first_five_evens.map(function (n) {
-      expect(n / 2).toBe(Math.floor(n / 2));
-    });
-  });
-  
-  it('reduces', function () {
-    var first_twenty_naturals = Stream.range(1, 20);
-    var twentieth_triangular_number_w_initial = first_twenty_naturals.reduce(function (prior, current) {
-      return prior + current;
-    }, 0);
 
-    var twentieth_triangular_number = first_twenty_naturals.reduce(function (prior, current) {
-      return prior + current;
-    });
-    
-    expect(twentieth_triangular_number).toBe(210);
-    expect(twentieth_triangular_number_w_initial).toBe(210);
+  it('filters', () => {
+    const firstTenNaturals = Stream.range(1, 10);
+    const firstFiveEvens = firstTenNaturals.filter(n => (n % 2 === 0));
+
+    expect(firstFiveEvens.length()).toBe(5);
+    firstFiveEvens.map(n => expect(n / 2).toBe(Math.floor(n / 2)));
   });
 
-  it('dropsWhile', function () {
-    var some_numbers = Stream.make(-5, -8, -2, 34, 10, -2);
-    var remaining_numbers = some_numbers.dropWhile(function (x) { return x<0; });
+  it('reduces', () => {
+    const firstTwentyNaturals = Stream.range(1, 20);
+    const twentiethTriangularNumberWInitial =
+    firstTwentyNaturals.reduce((prior, current) => prior + current, 0);
 
-    expect(remaining_numbers.head()).toBe(34);
-    expect(remaining_numbers.item(1)).toBe(10);
-    expect(remaining_numbers.item(2)).toBe(-2);
+    const twentiethTriangularNumber =
+    firstTwentyNaturals.reduce((prior, current) => prior + current);
+
+    expect(twentiethTriangularNumber).toBe(210);
+    expect(twentiethTriangularNumberWInitial).toBe(210);
   });
 
-  it('dropsWhile on empty streams', function () {
-    var s = new Stream();
-    expect(s.dropWhile(function (x) { return true; }).empty()).toBeTruthy();
+  it('dropsWhile', () => {
+    const someNumbers = Stream.make(-5, -8, -2, 34, 10, -2);
+    const remainingNumbers = someNumbers.dropWhile(x => x < 0);
+
+    expect(remainingNumbers.head()).toBe(34);
+    expect(remainingNumbers.item(1)).toBe(10);
+    expect(remainingNumbers.item(2)).toBe(-2);
   });
 
-  it('dropsWhile on exhausted streams', function () {
-    var some_numbers = Stream.make(-5, -8, -2, 34, 10, -2);
-    expect(some_numbers.dropWhile(function (x) { return true; }).empty()).toBeTruthy();
+  it('dropsWhile on empty streams', () => {
+    const s = new Stream();
+    expect(s.dropWhile(() => true).empty()).toBeTruthy();
   });
 
-  it('takesWhile', function () {
-    var some_numbers = Stream.make(-5, -8, -2, 34, 10, -2);
-    var remaining_numbers = some_numbers.takeWhile(function (x) { return x<0; });
-
-    expect(remaining_numbers.head()).toBe(-5);
-    expect(remaining_numbers.item(1)).toBe(-8);
-    expect(remaining_numbers.item(2)).toBe(-2);
+  it('dropsWhile on exhausted streams', () => {
+    const someNumbers = Stream.make(-5, -8, -2, 34, 10, -2);
+    expect(someNumbers.dropWhile(() => true).empty()).toBeTruthy();
   });
 
-  it('takesWhile on empty streams', function () {
-    var s = new Stream();
-    expect(s.takeWhile(function (x) { return x>0; }).empty()).toBeTruthy();
+  it('takesWhile', () => {
+    const someNumbers = Stream.make(-5, -8, -2, 34, 10, -2);
+    const remainingNumbers = someNumbers.takeWhile(x => x < 0);
+
+    expect(remainingNumbers.head()).toBe(-5);
+    expect(remainingNumbers.item(1)).toBe(-8);
+    expect(remainingNumbers.item(2)).toBe(-2);
   });
 
-  it('zips', function () {
-    var a = Stream.make(4, 8, 12, 23, 5);
-    var b = Stream.make(2, 10, 5, 99, 100);
-    var c = Stream.make(-5, 63, 12, 43, 2);
-
-    var biggest_of_two = a.zip( Math.max, b );
-    var sum_of_four = a.zip( function(w, x, y, z) {
-      return w + x + y + z;
-    }, b, c, a );
-
-    expect(biggest_of_two.head()).toBe(4);
-    expect(biggest_of_two.item(1)).toBe(10);
-    expect(biggest_of_two.item(2)).toBe(12);
-    expect(biggest_of_two.item(3)).toBe(99);
-    expect(biggest_of_two.item(4)).toBe(100);
-
-    expect(sum_of_four.head()).toBe(5);
-    expect(sum_of_four.item(1)).toBe(89);
-    expect(sum_of_four.item(2)).toBe(41);
-    expect(sum_of_four.item(3)).toBe(188);
-    expect(sum_of_four.item(4)).toBe(112);
+  it('takesWhile on empty streams', () => {
+    const s = new Stream();
+    expect(s.takeWhile(x => x > 0).empty()).toBeTruthy();
   });
 
-  it('zips streams of different lengths', function () {
-    var a = Stream.make(4, 8, 12, 16);
-    var b = Stream.make(1, 12, 42);
+  it('zips', () => {
+    const a = Stream.make(4, 8, 12, 23, 5);
+    const b = Stream.make(2, 10, 5, 99, 100);
+    const c = Stream.make(-5, 63, 12, 43, 2);
 
-    var biggest_of_two1 = a.zip(Math.max, b);
-    var biggest_of_two2 = b.zip(Math.max, a);
+    const biggestOfTwo = a.zip(Math.max, b);
+    const sumOfFour = a.zip((w, x, y, z) => w + x + y + z, b, c, a);
 
-    expect(biggest_of_two1.length()).toBe(3);
-    expect(biggest_of_two2.length()).toBe(3);
+    expect(biggestOfTwo.head()).toBe(4);
+    expect(biggestOfTwo.item(1)).toBe(10);
+    expect(biggestOfTwo.item(2)).toBe(12);
+    expect(biggestOfTwo.item(3)).toBe(99);
+    expect(biggestOfTwo.item(4)).toBe(100);
 
-    expect(biggest_of_two1.head()).toBe(4);
-    expect(biggest_of_two1.item(1)).toBe(12);
-    expect(biggest_of_two1.item(2)).toBe(42);
+    expect(sumOfFour.head()).toBe(5);
+    expect(sumOfFour.item(1)).toBe(89);
+    expect(sumOfFour.item(2)).toBe(41);
+    expect(sumOfFour.item(3)).toBe(188);
+    expect(sumOfFour.item(4)).toBe(112);
+  });
 
-    expect(biggest_of_two2.head()).toBe(4);
-    expect(biggest_of_two2.item(1)).toBe(12);
-    expect(biggest_of_two2.item(2)).toBe(42);
+  it('zips streams of different lengths', () => {
+    const a = Stream.make(4, 8, 12, 16);
+    const b = Stream.make(1, 12, 42);
+
+    const biggestOfTwo1 = a.zip(Math.max, b);
+    const biggestOfTwo2 = b.zip(Math.max, a);
+
+    expect(biggestOfTwo1.length()).toBe(3);
+    expect(biggestOfTwo2.length()).toBe(3);
+
+    expect(biggestOfTwo1.head()).toBe(4);
+    expect(biggestOfTwo1.item(1)).toBe(12);
+    expect(biggestOfTwo1.item(2)).toBe(42);
+
+    expect(biggestOfTwo2.head()).toBe(4);
+    expect(biggestOfTwo2.item(1)).toBe(12);
+    expect(biggestOfTwo2.item(2)).toBe(42);
   });
 });
 
-describe('special numeric stream functions', function () {
-  it('sums', function () {
-    var first_twenty_naturals = Stream.range(1, 20);
-    var twentieth_triangular_number = first_twenty_naturals.sum();
-    expect(twentieth_triangular_number).toBe(210);
+describe('special numeric stream functions', () => {
+  it('sums', () => {
+    const firstTwentyNaturals = Stream.range(1, 20);
+    const twentiethTriangularNumber = firstTwentyNaturals.sum();
+    expect(twentiethTriangularNumber).toBe(210);
   });
-  
-  it('scales', function () {
-    var first_ten_naturals = Stream.range(1, 10);
-    var first_ten_evens = first_ten_naturals.scale(2);
-    
-    expect(first_ten_evens.length()).toBe(10);
-    expect(first_ten_evens.head()).toBe(2);
-    expect(first_ten_evens.item(9)).toBe(20);
-  })
+
+  it('scales', () => {
+    const firstTenNaturals = Stream.range(1, 10);
+    const firstTenEvens = firstTenNaturals.scale(2);
+
+    expect(firstTenEvens.length()).toBe(10);
+    expect(firstTenEvens.head()).toBe(2);
+    expect(firstTenEvens.item(9)).toBe(20);
+  });
 });
